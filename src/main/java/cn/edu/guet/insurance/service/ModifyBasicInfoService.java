6a45447fc59ec1c18e337a439ebe79f73f6d64bf
lkh <1459452560@qq.com>
2023-07-27 17:00:20 +0800

    地州区县对应

---
+++ src/main/java/cn/edu/guet/insurance/service/ModifyBasicInfoService.java
@@ -0,0 +1,20 @@
+package cn.edu.guet.insurance.service;
+
+import cn.edu.guet.insurance.bean.ModifyBasicInfo;
+import cn.edu.guet.insurance.bean.SearchModifyDTO;
+import cn.edu.guet.insurance.common.ResponseData;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 14594
+* @description 针对表【modify_basic_info(modify_basic_info)】的数据库操作Service
+* @createDate 2023-07-23 21:50:29
+*/
+public interface ModifyBasicInfoService extends IService<ModifyBasicInfo> {
+
+    IPage searchModify(SearchModifyDTO searchModifyDTO);
+
+
+
+}
---
+++ src/main/java/cn/edu/guet/insurance/service/impl/ModifyBasicInfoServiceImpl.java
@@ -0,0 +1,58 @@
+package cn.edu.guet.insurance.service.impl;
+
+import cn.edu.guet.insurance.bean.SearchModifyDTO;
+import cn.edu.guet.insurance.common.ResponseData;
+import cn.hutool.core.collection.CollectionUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import cn.edu.guet.insurance.bean.ModifyBasicInfo;
+import cn.edu.guet.insurance.service.ModifyBasicInfoService;
+import cn.edu.guet.insurance.mapper.ModifyBasicInfoMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+* @author 14594
+* @description 针对表【modify_basic_info(modify_basic_info)】的数据库操作Service实现
+* @createDate 2023-07-23 21:50:29
+*/
+@Service
+public class ModifyBasicInfoServiceImpl extends ServiceImpl<ModifyBasicInfoMapper, ModifyBasicInfo>
+    implements ModifyBasicInfoService{
+
+    @Autowired ModifyBasicInfoMapper modifyBasicInfoMapper;
+
+    @Override
+    public IPage searchModify(SearchModifyDTO searchModifyDTO) {
+        Page page = new Page();
+        page.setSize(searchModifyDTO.getPageSize());
+        page.setCurrent(searchModifyDTO.getCurrentPage());
+        QueryWrapper<ModifyBasicInfo> qw = new QueryWrapper();
+        if (searchModifyDTO.getPrefecture() != 0) {
+            qw.eq("prefecture", searchModifyDTO.getPrefecture());
+        }
+        if (searchModifyDTO.getCounty() != 0) {
+            qw.eq("county", searchModifyDTO.getCounty());
+        }
+        if (!StringUtils.isBlank(searchModifyDTO.getModifyProjectName())){
+            qw.like("modify_project_name", searchModifyDTO.getModifyProjectName());
+        }
+        if (CollectionUtil.isNotEmpty(searchModifyDTO.getOverWorkTime())){
+            qw.between("actual_completion_time",searchModifyDTO.getOverWorkTime().get(0),searchModifyDTO.getOverWorkTime().get(1));
+        }
+        IPage listResult =modifyBasicInfoMapper.selectPage(page, qw);
+
+        return listResult;
+    }
+
+
+}
+
+
+
+
