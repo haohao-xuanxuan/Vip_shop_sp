04893ae3b152ee7f42f97a9fec9b10c40f22265c
isme ahao <2359240114@qq.com>
2023-08-09 17:18:57 +0800

    index

--- src/main/java/cn/edu/guet/controller/IndexController.java
+++ src/main/java/cn/edu/guet/controller/IndexController.java
@@ -3,6 +3,7 @@ package cn.edu.guet.controller;
 
 import cn.edu.guet.common.ResponseData;
 import cn.edu.guet.service.IndexGoodsService;
+import cn.edu.guet.service.IndexiShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,16 @@ public class IndexController {
     @Autowired
     IndexGoodsService indexGoodsService;
 
+    @Autowired
+    IndexiShowService indexiShowService;
+
     @GetMapping("/getIndexGoods")
     public ResponseData getIndexGoods(){
         return indexGoodsService.getIndexGoods();
     }
 
+    @GetMapping("/getIndexShow")
+    public ResponseData getIndexShow(){
+        return indexiShowService.getIndexShow();
+    }
 }
